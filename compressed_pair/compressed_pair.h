bd560f38dffe84e1cc0ebf4eead6afa3632e9899
Logan Smith <logan@brainiumstudios.com>
2017-01-04 22:37:43 -0800

    improvements: - support non-default-constructible types (empty and non-empty) - support final empty types (by not attempting EBO) - remove constructors/operators that were stepping on compiler's toes - return rvalue references when appropriate - more tests

--- compressed_pair/compressed_pair.h
+++ compressed_pair/compressed_pair.h
@@ -5,25 +5,36 @@
 #include <functional>
 
 namespace detail {
+	template<typename T>
+	constexpr bool is_EBO_eligible = std::is_empty<T>::value && !std::is_final<T>::value;
+	
 	template<typename T, unsigned N, typename Enable = void>
 	struct compressed_pair_member;
 	
 	template<typename T, unsigned N>
-	struct compressed_pair_member<T, N, typename std::enable_if_t<std::is_empty<T>::value>> : T {
+	struct compressed_pair_member<T, N, typename std::enable_if_t<is_EBO_eligible<T>>> : T {
 		using type = T;
 		T& get() { return *this; }
 		const T& get() const { return *this; }
+		
+		compressed_pair_member() = default;
+		template<typename Tp = type>
+		compressed_pair_member(Tp&& t) : T{(Tp&&)t} {}
 		
 		template<typename Tp = type>
 		void set(Tp&& t) {}
 	};
 	
 	template<typename T, unsigned N>
-	struct compressed_pair_member<T, N, typename std::enable_if_t<!std::is_empty<T>::value>> {
+	struct compressed_pair_member<T, N, typename std::enable_if_t<!is_EBO_eligible<T>>> {
 		using type = T;
 		T member;
 		T& get() { return member; }
 		const T& get() const { return member; }
+		
+		compressed_pair_member() = default;
+		template<typename Tp = type>
+		compressed_pair_member(Tp&& t) : member((Tp&&)t) {}
 		
 		template<typename Tp = type>
 		void set(Tp&& t) {
@@ -44,56 +55,43 @@ public:
 	using second_type = T2;
 	using first_reference = T1&;
 	using second_reference = T2&;
+	using first_rvalue_reference = T1&&;
+	using second_rvalue_reference = T2&&;
 	using first_const_reference = const T1&;
 	using second_const_reference = const T2&;
 	
 	// POD types are not zero-initialized
+	template<typename F = first_member_type, typename S = second_member_type,
+		typename = std::enable_if_t<
+			std::is_default_constructible<F>::value &&
+			std::is_default_constructible<S>::value
+		>
+	>
 	compressed_pair() {}
 	
 	template<typename F = T1, typename S = T2>
-	compressed_pair(F&& x, S&& y) {
-		first_member_type::set((F&&)x);
-		second_member_type::set((S&&)y);
-	}
+	compressed_pair(F&& x, S&& y)
+	: first_member_type{(F&&)x}, second_member_type{(S&&)y} {}
 	
 	template<typename F = T1, typename S = T2>
-	explicit compressed_pair(T1&& x, std::enable_if_t<!std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, nat> = nat{}) {
-		first_member_type::set((T1&&)x);
-	}
+	explicit compressed_pair(T1&& x, std::enable_if_t<!std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, nat> = nat{})
+	: first_member_type{(T1&&)x} {}
+	
 	template<typename F = T1, typename S = T2>
-	explicit compressed_pair(T2&& y, std::enable_if_t<!std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, const nat&> = nat{}) {
-		second_member_type::set((T2&&)y);
-	}
+	explicit compressed_pair(T2&& y, std::enable_if_t<!std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, const nat&> = nat{})
+	: second_member_type{(T2&&)y} {}
+	
 	template<typename F = T1, typename S = T2>
-	explicit compressed_pair(T1&& x, std::enable_if_t<std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, nat> = nat{}) {
-		first_member_type::set((T1&&)x);
-		second_member_type::set((T1&&)(x));
-	}
+	explicit compressed_pair(T1&& x, std::enable_if_t<std::is_same<std::remove_cv_t<F>, std::remove_cv_t<S>>::value, nat> = nat{})
+	: first_member_type{(T1&&)x}, second_member_type{(T1&&)x} {}
 	
-	compressed_pair(const compressed_pair& other) {
-		first_member_type::set(other.first());
-		second_member_type::set(other.second());
-	}
-	compressed_pair& operator=(const compressed_pair& other) {
-		first_member_type::set(other.first());
-		second_member_type::set(other.second());
-		return *this;
-	}
-	compressed_pair(compressed_pair&& other) {
-		first_member_type::set(std::move(other.first()));
-		second_member_type::set(std::move(other.second()));
-	}
-	compressed_pair& operator=(compressed_pair&& other) {
-		first_member_type::set(std::move(other.first()));
-		second_member_type::set(std::move(other.second()));
-		return *this;
-	}
+	first_reference first() & { return first_member_type::get(); }
+	first_rvalue_reference first() && { return std::move(first_member_type::get()); }
+	first_const_reference first() const& { return first_member_type::get(); }
 	
-	first_reference first() { return first_member_type::get(); }
-	first_const_reference first() const { return first_member_type::get(); }
-	
-	second_reference second() { return second_member_type::get(); }
-	second_const_reference second() const { return second_member_type::get(); }
+	second_reference second() & { return second_member_type::get(); }
+	second_rvalue_reference second() && { return std::move(second_member_type::get()); }
+	second_const_reference second() const& { return second_member_type::get(); }
 };
 
 #endif
--- compressed_pair/main.cpp
+++ compressed_pair/main.cpp
@@ -2,38 +2,40 @@
 
 #include <iostream>
 #include <sstream>
-#include <vector>
+#include <memory>
 
 struct empty_type {};
 struct empty_type2 {};
 struct non_empty_type { empty_type e; };
 
 struct copy_move_type {
-	copy_move_type() {
-		std::cout << "default constructor\n";
-	}
-	copy_move_type(const copy_move_type&) {
-		std::cout << "copy constructor\n";
-	}
-	copy_move_type& operator=(const copy_move_type&) {
-		std::cout << "copy assignment\n";
-		return *this;
-	}
-	copy_move_type(copy_move_type&&) {
-		std::cout << "move constructor\n";
-	}
-	copy_move_type& operator=(copy_move_type&&) {
-		std::cout << "move assignment\n";
-		return *this;
-	}
-	int non_empty;
+	copy_move_type() : copies{0}, moves{0} {}
+	copy_move_type(const copy_move_type&) { ++copies; }
+	copy_move_type& operator=(const copy_move_type&) { ++copies; return *this; }
+	copy_move_type(copy_move_type&&) { ++moves; }
+	copy_move_type& operator=(copy_move_type&&) { ++moves; return *this; }
+	unsigned copies, moves;
 };
 
-std::ostream& operator <<(std::ostream& stream, const empty_type& e) {
+struct empty_non_default_constructible {
+	empty_non_default_constructible() = delete;
+	empty_non_default_constructible(int) {}
+};
+
+struct non_empty_non_default_constructible {
+	non_empty_non_default_constructible() = delete;
+	non_empty_non_default_constructible(int) {}
+	int x;
+};
+
+struct empty_final final {};
+struct non_empty_final final { empty_final e; };
+
+std::ostream& operator<<(std::ostream& stream, const empty_type& e) {
 	return stream << "[empty type]";
 }
 
-std::ostream& operator <<(std::ostream& stream, const copy_move_type& c) {
+std::ostream& operator<<(std::ostream& stream, const copy_move_type& c) {
 	return stream << "[copy move type]";
 }
 
@@ -44,21 +46,25 @@ std::string to_string(const compressed_pair<T, U>& pair) {
 	return stream.str();
 }
 
-#define RUNTIME_ASSERT(condition) do { if (!(condition)) throw; } while ( false )
+#define RUNTIME_ASSERT(condition) do { if ( !(condition) ) throw; } while(false)
 
 void tests() {
 	// sanity checks
-	static_assert(std::is_empty<empty_type>{} && std::is_empty<empty_type2>{}, "");
-	static_assert(!std::is_empty<int>{}, "");
-	static_assert(!std::is_empty<non_empty_type>{}, "");
-	static_assert(sizeof(non_empty_type) == 1, "");
+	static_assert(std::is_empty<empty_type>{} && std::is_empty<empty_type2>{} && std::is_empty<empty_non_default_constructible>{} && std::is_empty<empty_final>{}, "");
+	static_assert(!std::is_empty<int>{} && !std::is_empty<non_empty_type>{} && !std::is_empty<non_empty_non_default_constructible>{} && !std::is_empty<non_empty_final>{}, "");
 	
 	// size
 	static_assert(sizeof(compressed_pair<int, int>) == 2*sizeof(int), "");
 	static_assert(sizeof(compressed_pair<int, empty_type>) == sizeof(int), "");
 	static_assert(sizeof(compressed_pair<empty_type, int>) == sizeof(int), "");
+	static_assert(sizeof(compressed_pair<empty_type, empty_type>) == 2, "");
 	static_assert(sizeof(compressed_pair<empty_type, empty_type2>) == 1, "");
 	static_assert(sizeof(compressed_pair<empty_type, empty_type>) == sizeof(compressed_pair<empty_type2, empty_type2>), "");
+	
+	// construction
+	static_assert(!std::is_default_constructible<compressed_pair<non_empty_non_default_constructible, int>>::value, "");
+	static_assert(!std::is_default_constructible<compressed_pair<int, empty_non_default_constructible>>::value, "");
+	static_assert( std::is_default_constructible<compressed_pair<int, char>>::value, "");
 	
 	{
 		// two-argument constructor
@@ -71,10 +77,12 @@ void tests() {
 		RUNTIME_ASSERT(to_string(test) == "x, x");
 	}
 	{
+		// single-argument constructor, first type
 		auto test = compressed_pair<char, empty_type>{'x'};
 		RUNTIME_ASSERT(to_string(test) == "x, [empty type]");
 	}
 	{
+		// single-argument constructor, second type
 		auto test = compressed_pair<empty_type, unsigned>{0u};
 		RUNTIME_ASSERT(to_string(test) == "[empty type], 0");
 	}
@@ -97,6 +105,18 @@ void tests() {
 		
 		auto copy = test;
 		auto&& move = std::move(copy);
+		RUNTIME_ASSERT(to_string(move) == to_string(test));
+	}
+	{
+		// crazy stuff
+		compressed_pair<non_empty_non_default_constructible, std::string> t1{42};
+		compressed_pair<empty_non_default_constructible, empty_type> t2{42};
+		compressed_pair<non_empty_non_default_constructible, empty_non_default_constructible> t3{42, 42};
+		compressed_pair<empty_final, int> t4;
+		compressed_pair<empty_final, non_empty_final> t5;
+		compressed_pair<int, std::unique_ptr<empty_non_default_constructible>> t6(std::make_unique<empty_non_default_constructible>(42));
+		
+		auto t7 = std::move(t6);
 	}
 }
 
